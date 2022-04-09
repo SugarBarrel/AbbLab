4439d7f69be9efa5454419cadc6bb388521cdbc1
Abbysssal <hillankeyxl2@yandex.ru>
2022-04-09 15:53:51 +0700

    ✨ Add ReadOnlyCollection.Empty<T>()

---
+++ AbbLab.Extensions/ReadOnlyCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using JetBrains.Annotations;
+
+namespace AbbLab.Extensions
+{
+    public static class ReadOnlyCollection
+    {
+        [Pure] public static ReadOnlyCollection<T> Empty<T>() => Typed<T>.Empty;
+
+        private static class Typed<T>
+        {
+            public static readonly ReadOnlyCollection<T> Empty = new ReadOnlyCollection<T>(Array.Empty<T>());
+        }
+    }
+}
