0c9877589e6c3ae971964d4c5ff446a04e6bb680
Abbysssal <hillankeyxl2@yandex.ru>
2022-04-18 10:20:39 +0700

    ✨ Add Util.Fail() methods

---
+++ AbbLab.Extensions/Util.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace AbbLab.Extensions
+{
+    public static class Util
+    {
+        [Pure] public static bool Fail<T>(out T? result)
+        {
+            result = default;
+            return false;
+        }
+        [Pure] public static bool Fail<T1, T2>(out T1? result1, out T2? result2)
+        {
+            result1 = default;
+            result2 = default;
+            return false;
+        }
+        [Pure] public static bool Fail<T1, T2, T3>(out T1? result1, out T2? result2, out T3? result3)
+        {
+            result1 = default;
+            result2 = default;
+            result3 = default;
+            return false;
+        }
+
+        [Pure] [return: NotNullIfNotNull("returnValue")]
+        public static TReturn? Fail<TReturn, T>(TReturn? returnValue, out T? result)
+        {
+            result = default;
+            return returnValue;
+        }
+        [Pure] [return: NotNullIfNotNull("returnValue")]
+        public static TReturn? Fail<TReturn, T1, T2>(TReturn? returnValue, out T1? result1, out T2? result2)
+        {
+            result1 = default;
+            result2 = default;
+            return returnValue;
+        }
+        [Pure] [return: NotNullIfNotNull("returnValue")]
+        public static TReturn? Fail<TReturn, T1, T2, T3>(TReturn? returnValue, out T1? result1, out T2? result2, out T3? result3)
+        {
+            result1 = default;
+            result2 = default;
+            result3 = default;
+            return returnValue;
+        }
+
+    }
+}
