544201261e7327e7c0e08feb3235b05e4728a952
Abbysssal <hillankeyxl2@yandex.ru>
2022-06-24 15:09:37 +0700

    ✨ Add string StringBuilder.ToStringAndClear() extension method

---
+++ AbbLab.Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using JetBrains.Annotations;
+
+namespace AbbLab.Extensions
+{
+    public static class StringBuilderExtensions
+    {
+        [Pure] public static string ToStringAndClear(this StringBuilder sb)
+        {
+            string res = sb.ToString();
+            sb.Clear();
+            return res;
+        }
+    }
+}
