ae0438f9e5bd3f369195a0e5db2be9ad72a3940d
Abbysssal <hillankeyxl2@yandex.ru>
2022-07-14 16:20:38 +0700

    ✅ Add semantic version formatting tests

---
+++ AbbLab.SemanticVersioning.Tests/SemanticVersionTests.Formatting.Fixtures.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+
+namespace AbbLab.SemanticVersioning.Tests
+{
+    public partial class SemanticVersionTests
+    {
+        public static IEnumerable<object?[]> EnumerateFormatFixtures()
+        {
+            List<FormatFixture> fixtures = new List<FormatFixture>();
+            FormatFixture New(string input, string? format = null)
+            {
+                FormatFixture fixture = new FormatFixture(input, format);
+                fixtures.Add(fixture);
+                return fixture;
+            }
+
+
+
+            // Default format
+            New("1.2.3");
+            New("12.345.6789");
+            New("123.45.6-alpha.6");
+            New("1.2.3+BUILD.007");
+            New("1.2.3-alpha.0+DEV.00");
+
+            // simple format
+            New("1.2.3", "M.m.p").Returns("1.2.3");
+            New("1.2.3-pre+build", "M.m.p-rr+dd").Returns("1.2.3-pre+build");
+
+            // identifiers
+            New("1.2.3", "M.m.p-rr+dd").Returns("1.2.3");
+            New("1.2.3-pre+build", "M.m.p").Returns("1.2.3");
+            New("1.2.3-pre+build", "M.m.p+dd").Returns("1.2.3+build");
+
+            // weird orders
+            New("1.2.3-pre.2+build.00", "dd+rr-p.m.M").Returns("build.00+pre.2-3.2.1");
+
+            // optional components
+            New("1.2.3-beta", "M.m.pp-rr").Returns("1.2.3-beta");
+            New("1.2.0-beta", "M.m.pp-rr").Returns("1.2-beta");
+            New("1.2.0-beta", "M.mm.p-rr").Returns("1.2.0-beta");
+            New("1.0.0-beta", "M.mm.p-rr").Returns("1.0-beta");
+            New("1.2.3-beta", "M.mm.pp-rr").Returns("1.2.3-beta");
+            New("1.2.0-beta", "M.mm.pp-rr").Returns("1.2-beta");
+            New("1.0.0-beta", "M.mm.pp-rr").Returns("1-beta");
+            New("1.0.3-beta", "M.mm.pp-rr").Returns("1.0.3-beta");
+
+            // Escaped sequences
+            New("1.2.3-alpha", @"\M.\m.\p-\r\r: M.m.p-rr").Returns(@"M.m.p-rr: 1.2.3-alpha");
+            New("1.2.3-alpha", "'M.m.p-rr': M.m.p-rr").Returns("M.m.p-rr: 1.2.3-alpha");
+            New("1.2.3-alpha", "\"M.m.p-rr\": M.m.p-rr").Returns("M.m.p-rr: 1.2.3-alpha");
+            New("1.2.3-alpha", "'M.m.p-rr: M.m.p-rr").Throws("The format string contains an unclosed quote ('\\'', '\"').");
+            New("1.2.3-alpha", "\"M.m.p-rr: M.m.p-rr").Throws("The format string contains an unclosed quote ('\\'', '\"').");
+
+            // indexed identifiers
+            New("1.2.3", "-r0.r1.r2").Returns("");
+            New("1.2.3-beta", "-r0.r1.r2").Returns("-beta");
+            New("1.2.3-alpha.3.beta.5.gamma", "-r0.r1.r2").Returns("-alpha.3.beta");
+            New("1.2.3-alpha.3.beta.5.gamma", "-r2.r").Returns("-beta.5");
+            New("1.2.3-alpha.3.beta.5.gamma", "-r2.rr").Returns("-beta.5.gamma");
+            New("1.2.3-alpha.3.beta.5.gamma", "-r4.r").Returns("-gamma");
+            New("1.2.3-alpha.3.beta.5.gamma", "-r4.rr").Returns("-gamma");
+            New("1.2.3-alpha.3.beta.5.gamma", "-r4.r0.rr").Returns("-gamma.alpha.3.beta.5.gamma");
+
+            New("1.2.3", "+d0.d1.d2").Returns("");
+            New("1.2.3+build", "+d0.d1.d2").Returns("+build");
+            New("1.2.3+build.007.dev.2.test", "+d0.d1.d2").Returns("+build.007.dev");
+            New("1.2.3+build.007.dev.2.test", "+d2.d").Returns("+dev.2");
+            New("1.2.3+build.007.dev.2.test", "+d2.dd").Returns("+dev.2.test");
+            New("1.2.3+build.007.dev.2.test", "+d4.d").Returns("+test");
+            New("1.2.3+build.007.dev.2.test", "+d4.dd").Returns("+test");
+            New("1.2.3+build.007.dev.2.test", "+d4.d0.dd").Returns("+test.build.007.dev.2.test");
+
+
+
+            return TestUtil.CreateFixtures(fixtures.ToArray());
+        }
+
+    }
+}
---
+++ AbbLab.SemanticVersioning.Tests/SemanticVersionTests.Formatting.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using Xunit;
+
+namespace AbbLab.SemanticVersioning.Tests
+{
+    public partial class SemanticVersionTests
+    {
+        [Theory, MemberData(nameof(EnumerateFormatFixtures))]
+        public void FormattingTests(FormatFixture fixture)
+        {
+            Output.WriteLine($"Formatting v{fixture.Version}");
+            Output.WriteLine($"Using {(fixture.Format is not null ? $"`{fixture.Format}`" : "default format")}");
+            Output.WriteLine($"Expected: {(fixture.Expected is not null ? $"`{fixture.Expected}`" : "format exception")}");
+
+            SemanticVersion version = SemanticVersion.Parse(fixture.Version);
+
+            if (fixture.Format is null)
+            {
+                AssertEx.Identical(new Func<string>[]
+                {
+                    () => version.ToString(),
+                    () => version.ToString(null),
+                    () => version.ToString("G"),
+                    () => version.ToString("g"),
+                    () => ((IFormattable)version).ToString(null, null),
+                    () => ((IFormattable)version).ToString("G", null),
+                    () => ((IFormattable)version).ToString("g", null),
+                }, fixture.Assert);
+            }
+            else
+            {
+                AssertEx.Identical(new Func<string>[]
+                {
+                    () => version.ToString(fixture.Format),
+                    () => ((IFormattable)version).ToString(fixture.Format, null),
+                }, fixture.Assert);
+            }
+        }
+
+        public class FormatFixture
+        {
+            public FormatFixture(string version, string? format = null)
+            {
+                Version = version;
+                Format = format;
+                if (format is null) Expected = version;
+            }
+            public string Version { get; }
+            public string? Format { get; }
+            public string? Expected { get; set; }
+
+            public bool IsValid => ErrorType is null;
+            public Type? ErrorType { get; private set; }
+            public string? ErrorMessage { get; private set; }
+
+            public FormatFixture Returns(string expected)
+            {
+                Expected = expected;
+                return this;
+            }
+            public FormatFixture Throws(string message) => Throws<FormatException>(message);
+            public FormatFixture Throws<TException>(string message)
+            {
+                ErrorType = typeof(TException);
+                ErrorMessage = message;
+                return this;
+            }
+
+            public void Assert(string? result, Exception? exception)
+            {
+                if (IsValid)
+                {
+                    Xunit.Assert.NotNull(result);
+                    Xunit.Assert.Equal(Expected, result);
+                }
+                else
+                {
+                    Xunit.Assert.NotNull(exception);
+                    Xunit.Assert.Equal(ErrorType, exception!.GetType());
+                    Xunit.Assert.StartsWith(ErrorMessage!, exception.Message);
+                }
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append('v').Append(Version);
+                if (Format is not null) sb.Append(" {").Append(Format).Append('}');
+                return sb.ToString();
+            }
+
+        }
+    }
+}
