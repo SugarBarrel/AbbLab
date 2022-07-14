8a140599fd72b9bc677e8a47bd8dacc3a916e8f5
Abbysssal <hillankeyxl2@yandex.ru>
2022-07-15 06:46:37 +0700

    ✅ Add semantic version comparison tests

---
+++ AbbLab.SemanticVersioning.Tests/SemanticVersionTests.Comparison.Fixtures.cs
@@ -0,0 +1,33 @@
+﻿namespace AbbLab.SemanticVersioning.Tests
+{
+    public partial class SemanticVersionTests
+    {
+        public static readonly string?[] ComparisonFixtures =
+        {
+            null,
+            "0.0.0-0",
+            "0.0.0-alpha",
+            "0.0.0",
+            "0.0.1-0",
+            "0.0.1",
+            "0.0.9",
+            "0.1.0",
+            "0.2.0-pre",
+            "0.2.0-pre.0",
+            "0.2.0-pre.1",
+            "0.2.0-pre.alpha",
+            "0.2.0-pre.alpha-beta",
+            "1.0.0-0",
+            "1.0.0-beta",
+            "1.0.0-beta.0",
+            "1.0.0",
+            "1.1.0",
+            "1.2147483647.0",
+            "2147483647.1.2-0",
+            "2147483647.1.2",
+            "2147483647.2147483647.2147483647-0",
+            "2147483647.2147483647.2147483647",
+        };
+
+    }
+}
---
+++ AbbLab.SemanticVersioning.Tests/SemanticVersionTests.Comparison.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+
+namespace AbbLab.SemanticVersioning.Tests
+{
+    public partial class SemanticVersionTests
+    {
+        [Fact]
+        public void ComparisonTest()
+        {
+            int length = ComparisonFixtures.Length;
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < length; j++)
+                {
+                    static SemanticVersion? Parse(string? str) => str is null ? null : SemanticVersion.Parse(str);
+
+                    SemanticVersion? a = Parse(ComparisonFixtures[i]);
+                    SemanticVersion? b = Parse(ComparisonFixtures[j]);
+
+                    if (a is not null)
+                    {
+                        Assert.Equal(i.Equals(j), a.Equals(b));
+                        Assert.Equal(i.Equals(j), a.Equals((object?)b));
+
+                        Assert.Equal(Math.Sign(i.CompareTo(j)), Math.Sign(a.CompareTo(b)));
+                        Assert.Equal(Math.Sign(((IComparable)i).CompareTo(j)), Math.Sign(((IComparable)a).CompareTo(b)));
+                        Assert.Throws<ArgumentException>(() => ((IComparable)a).CompareTo("not a version"));
+
+                        if (i == j)
+                        {
+                            Assert.True(a.Equals(a));
+                            Assert.Equal(0, a.CompareTo(a));
+                            Assert.Equal(a.GetHashCode(), b!.GetHashCode());
+                        }
+                    }
+
+                    Assert.Equal(i == j, a == b);
+                    Assert.Equal(i != j, a != b);
+
+                    Assert.Equal(i > j, a > b);
+                    Assert.Equal(i < j, a < b);
+                    Assert.Equal(i >= j, a >= b);
+                    Assert.Equal(i <= j, a <= b);
+                }
+        }
+
+    }
+}
